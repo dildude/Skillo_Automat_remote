cbed9e262d648b2faddfed54c1a1421701374a9f
Mincho K <vbass@abv.bg>
2024-02-28 14:28:07 +0200

    Update PracticingGit.java
    change in master that doesn't exist in branch 1

--- src/hw3/PracticingGit.java
+++ src/hw3/PracticingGit.java
@@ -3,5 +3,7 @@ package hw3;
 public class PracticingGit {
     public static void main(String[] args) {
         System.out.println("change one");
+        System.out.println("potential conflict making change in master ");
+        System.out.println(" that does not exist in new branch 1");
     }
 }
