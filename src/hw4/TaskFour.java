db3f42f9738ce89cbf162e7e828e25e2ebe5c51d
Mincho K <vbass@abv.bg>
2024-03-03 12:29:13 +0200

    adding solution for task four

---
+++ src/hw4/TaskFour.java
@@ -0,0 +1,18 @@
+package hw4;
+
+public class TaskFour {
+    public static void main(String[] args) {
+
+        /* Whit goggle and gemini help*/
+
+        int[] nums = {30, 42, 7, 103, 11, 69};
+        int setLargest = nums[0]; // selecting the first number in the array for the largest
+
+        for (int i = 1; i < nums.length; i++){
+            if (nums[i] > setLargest){
+                setLargest = nums[i];  // updating the largest num if there is a bigger one from the initial
+            }
+        }
+
+    }
+}
