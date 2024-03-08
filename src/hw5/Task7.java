60a2ebd6231c3d42c81b7a3872f327ed136d0fdf
Mincho K <vbass@abv.bg>
2024-03-08 18:00:44 +0200

    putting some new variant of task 7 in hw5

--- src/hw5/Task7.java
+++ src/hw5/Task7.java
@@ -9,24 +9,44 @@ public class Task7 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the word you want to validate:");
         String input = scanner.nextLine();
+        char[] inpuyArray = input.toCharArray();
+        char[] reservsedInput = new char[inpuyArray.length];
+        boolean isEqual = true;
 
-        boolean isPalindrome = true;
-        int left = 0;
-        int right = input.length() - 1;
-        while (left < right) {
-            if (input.charAt(left) != input.charAt(right)) {
-                isPalindrome = false;
+        for (int i = inpuyArray.length - 1, j = 0; i >= 0 ; i--, j++) {
+            reservsedInput[j] = inpuyArray[i];
+        }
+        for (int i = 0; i < inpuyArray.length; i++) {
+            if (inpuyArray[i] != reservsedInput[i]){
+                isEqual = false;
                 break;
             }
-            left++;
-            right--;
+
+        }
+        if (isEqual){
+            System.out.println("The word is palindrom");
+        }else {
+            System.out.println("The word is not");
         }
 
-        if (isPalindrome) {
-            System.out.println(input + " is a palindrome");
-        } else {
-            System.out.println(input + " is not a palindrome");
-        }
+
+//        boolean isPalindrome = true;
+//        int left = 0;
+//        int right = input.length() - 1;
+//        while (left < right) {
+//            if (input.charAt(left) != input.charAt(right)) {
+//                isPalindrome = false;
+//                break;
+//            }
+//            left++;
+//            right--;
+//        }
+//
+//        if (isPalindrome) {
+//            System.out.println(input + " is a palindrome");
+//        } else {
+//            System.out.println(input + " is not a palindrome");
+//        }
     }
 }
 
