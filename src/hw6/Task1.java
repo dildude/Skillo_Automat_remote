f14c5eabe102a551defb94c03e027c7c65051521
Mincho K <vbass@abv.bg>
2024-03-10 19:09:34 +0200

    adding task one with the conditions

---
+++ src/hw6/Task1.java
@@ -0,0 +1,34 @@
+package hw6;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        findingSmallestNum();
+
+    }
+
+    static void findingSmallestNum() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbs = new int[3];
+
+        for (int i = 0; i < numbs.length; i++) {
+            System.out.println("Select the the a number:");
+            numbs[i] = scanner.nextInt();
+        }
+
+        int theSmallestNum = numbs[0];
+
+        for (int i = 0; i < numbs.length; i++) {
+            if(numbs[i] < theSmallestNum){
+                theSmallestNum = numbs[i];
+            }
+        }
+
+        System.out.println("The smallest number is:" + theSmallestNum );
+
+    }
+}
---
+++ src/hw6/TaskCondit.java
@@ -0,0 +1,20 @@
+package hw6;
+
+public class TaskCondit {
+
+    /* 1. Write a Java method to find the smallest number among three numbers
+
+2. Write a Java method to compute the average of three numbers.
+
+3. Write a Java method to display the middle character of a string.
+Note:
+a) If the length of the string is even there will be two middle characters.
+b) If the length of the string is odd there will be one middle character.
+ ?. Write a Java method to count all words in a string.
+4. Write a method named isEven that accepts an int argument.
+ The method should return true if the argument is even, or false otherwise.
+
+5. A takes as input a non-negative integer and returns true if the number is a palindrome;
+Non-negative integer is called a palindrome if it reads forward and backward in the same way.
+ For example, the numbers 5, 121, 3443, and 123454321 are palindromes.*/
+}
